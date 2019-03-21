6196abddb7a635cd52d1666fc1f4afcd9c3bcb81
rhh72 <34271599+rhh72@users.noreply.github.com>
2019-03-21 11:16:03 -0700

    Updates

---
+++ precache-manifest.4b58caa48360594a4b4ec984903dc048.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "97f72510a2115578557f",
+    "url": "/octoturbo/static/js/runtime~main.b54754b5.js"
+  },
+  {
+    "revision": "a62fc4346d85c0cc3a81",
+    "url": "/octoturbo/static/js/main.6989db5a.chunk.js"
+  },
+  {
+    "revision": "02053234031beec21125",
+    "url": "/octoturbo/static/js/2.28db9f36.chunk.js"
+  },
+  {
+    "revision": "a62fc4346d85c0cc3a81",
+    "url": "/octoturbo/static/css/main.e3d7fc26.chunk.css"
+  },
+  {
+    "revision": "02053234031beec21125",
+    "url": "/octoturbo/static/css/2.2ad05b4b.chunk.css"
+  },
+  {
+    "revision": "d7d0b639814128ce96fe3edb10bc90a0",
+    "url": "/octoturbo/index.html"
+  }
+];
